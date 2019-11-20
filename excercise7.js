1af82a4e8a3d39b9f7085761aa76492ab7da55b8
purandrar <andra_pur@yahoo.com>
2019-11-20 16:28:10 +0700

    excercise 7 tugas 2 final

--- excercise7.js
+++ excercise7.js
@@ -12,9 +12,8 @@ console.log("");
 
 //tugas 2
 console.log("tugas 2");
-var rows2 = 9;
+var rows2 = 5;
 var row = rows2;
-
 while(rows2 > 0){
     var sterne = "*"
     for(var i = 0; i < row; i++){
@@ -30,7 +29,7 @@ console.log("");
 
 //tugas 3
 console.log("tugas 3");
-var rows3 = 9;
+var rows3 = 6;
 for(var i = 1; i <= rows3; i++){
     var bintang="*";	
     for (var j = 1; j < i; j++){
